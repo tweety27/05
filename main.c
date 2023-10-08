781c637fa941bdde2fa366180c042a356038c9c9
tweety27 <135826584+tweety27@users.noreply.github.com>
2023-10-08 23:07:04 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,16 +1,36 @@
 #include <stdio.h>
 
 int main() {
-    int num;
-    int sum = 0;
+    int num1, num2;
+    char operator;
+    int result;
 
-    printf("input a number: ");
-    scanf("%d", &num);
-    
-    for (int i = 1; i <= num; i++) {
-        sum += i;
+    printf("enter the calculation : ");
+    scanf("%d %c %d", &num1, &operator, &num2);
+
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            if (num2 == 0) {
+                printf("오류: 나누는 수가 0입니다.\n");
+                return 1;
+            }
+            result = num1 / num2;
+            break;
+        default:
+            printf("오류: 잘못된 연산자입니다.\n");
+            return 1;
     }
-    printf("The result is %d\n", sum);
+
+    printf("= %d\n", result);
 
     return 0;
 }
