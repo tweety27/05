1c6b919ac5fea55890d466fab8ad91e0755741cf
tweety27 <135826584+tweety27@users.noreply.github.com>
2023-10-08 23:14:42 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,36 +1,25 @@
 #include <stdio.h>
 
 int main() {
-    int num1, num2;
-    char operator;
-    int result;
+    const int answer = 59;
 
-    printf("enter the calculation : ");
-    scanf("%d %c %d", &num1, &operator, &num2);
+    int guess;
+    int attempts = 0;
 
-    switch (operator) {
-        case '+':
-            result = num1 + num2;
-            break;
-        case '-':
-            result = num1 - num2;
-            break;
-        case '*':
-            result = num1 * num2;
-            break;
-        case '/':
-            if (num2 == 0) {
-                printf("오류: 나누는 수가 0입니다.\n");
-                return 1;
-            }
-            result = num1 / num2;
-            break;
-        default:
-            printf("오류: 잘못된 연산자입니다.\n");
-            return 1;
-    }
+    do {
+        printf("Guess a number: ");
+        scanf("%d", &guess);
+        attempts++;
 
-    printf("= %d\n", result);
+        if (guess < answer) {
+            printf("low!\n");
+        } else if (guess > answer) {
+            printf("high!\n");
+        } else {
+            printf("Congratulation! trials: %d\n", attempts);
+        }
+    } while (guess != answer);
 
     return 0;
 }
+
