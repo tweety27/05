232e37c1dfa2fcaa0b40472c9647325bd73c3e07
tweety27 <135826584+tweety27@users.noreply.github.com>
2023-10-08 22:53:49 +0900

    Update main.c

--- main.c
+++ main.c
@@ -6,12 +6,11 @@ int main() {
     printf("정수 하나를 입력하시오: ");
     scanf("%d", &num);
 
-    if (num > 0) {
-        printf("양수입니다.\n");
-    } else if (num < 0) {
-        printf("음수입니다.\n");
+    if (num >= 0) {
+        printf("절대값은 %d입니다.\n", num);
     } else {
-        printf("0입니다.\n");
+        num = -num;
+        printf("절대값은 %d입니다.\n", num);
     }
 
     return 0;
